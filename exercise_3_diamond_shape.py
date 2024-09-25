1d1d6b40197f49977ad0dc1b45a2f78c1c781965
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 13:58:16 +0800

    Pseudocode for the Program

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -1,11 +1,13 @@
+# Creating Function of the Diamond Shape
 def print_diamond(n):
+    # Condition for the Odd Integer
     if n % 2 == 0:
-        print("Please Provide an odd integer.")
-
+        return "Please Provide an odd integer."
+    # Creating the Upper Part of the Diamond
     for i in range(n // 2 + 1):
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
-
+    # Creating the Lower Part of the Diamond
     for i in range(n // 2 - 1, -1, -1):
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
-
+# Calling the Function
 print_diamond(5)     
